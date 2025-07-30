9dad47625bc51988143dcfd50a985d2e0a79ca26
Bruno Prado <bruno@francaprado.com>
2025-07-30 19:39:06 -0300

    chore: add initial files

---
+++ ForMeApp/ForMeApp/ForMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForMeAppApp.swift
+//  ForMeApp
+//
+//  Created by Bruno França do Prado on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ForMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
